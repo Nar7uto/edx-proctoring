c6e56f140d315f928e68a6fd08af284e1f1bc32f
Matt Hughes <mhughes@edx.org>
2018-11-15 15:25:08 -0500

    bug fixes
    removed old reference to unscoped JS global approach
    fixed `switch` fallthrough

--- edx_proctoring/static/proctoring/js/plugin/mockprock-provider.js
+++ edx_proctoring/static/proctoring/js/plugin/mockprock-provider.js
@@ -5,13 +5,13 @@ const handler = new MockProctoringEventHandler();
 self.addEventListener("message", (message) => {
   switch(message.data.type) {
     case 'startExam': {
-      handler.onStartExam().then(() => self.postMessage({type: 'examStarted'}))
+      return handler.onStartExam().then(() => self.postMessage({type: 'examStarted'}))
     }
     case 'endExam': {
-      handler.onEndExam().then(() => self.postMessage({type: 'examEnded'}))
+      return handler.onEndExam().then(() => self.postMessage({type: 'examEnded'}))
     }
     case 'ping': {
-      handler.onPing().then(() => self.postMessage({type: 'echo'}))
+      return handler.onPing().then(() => self.postMessage({type: 'echo'}))
     }
   }
 });
